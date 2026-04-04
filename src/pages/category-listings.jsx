cdd2e1347267d711b14f51ce22525ada35c8f9c6
oyuncukantinim <oyuncukantinim@gmail.com>
2026-04-05 02:33:11 +0300

    Fill category banner hero

--- src/pages/category-listings.jsx
+++ src/pages/category-listings.jsx
@@ -164,7 +164,7 @@ export default function CategoryListingsPage() {
       <div className="card overflow-hidden">
         {category?.banner_image ? (
           <div className="relative h-40 bg-slate-100">
-            <img src={category.banner_image} alt={category.name} className="h-full w-full object-contain" />
+            <img src={category.banner_image} alt={category.name} className="h-full w-full object-cover object-center" />
             <div className="absolute inset-0 flex items-center bg-gradient-to-r from-black/70 to-transparent px-8">
               <div>
                 <h1 className="text-3xl font-extrabold text-white">{category?.name}</h1>
